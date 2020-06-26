40e0e95b3652d843ac8b64348687297b932963a0
nisinghvmware <67397603+nisinghvmware@users.noreply.github.com>
2020-06-26 16:52:53 -0700

    Create main.tf for 10 null resources example

---
+++ scaleperf_10resources/main.tf
@@ -0,0 +1,60 @@
+resource "null_resource" "example1" { 
+  provisioner "local-exec" { 
+     command = "find ." 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example2" { 
+  provisioner "local-exec" { 
+     command = "ls" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example3" { 
+  provisioner "local-exec" { 
+     command = "df" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example4" { 
+  provisioner "local-exec" { 
+     command = "ls" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example5" { 
+  provisioner "local-exec" { 
+     command = "pwd" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example6" { 
+  provisioner "local-exec" { 
+     command = "man" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example7" { 
+  provisioner "local-exec" { 
+     command = "find ." 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example8" { 
+  provisioner "local-exec" { 
+     command = "ls -a" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example9" { 
+  provisioner "local-exec" { 
+     command = "df" 
+     interpreter = ["sh", "-c"]    
+  } 
+}
+resource "null_resource" "example10" { 
+  provisioner "local-exec" { 
+     command = "pwd" 
+     interpreter = ["sh", "-c"]    
+  } 
+}

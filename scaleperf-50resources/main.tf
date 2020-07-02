8209151b2d99a7f09a6b7b45dff871b3342543a1
nisinghvmware <67397603+nisinghvmware@users.noreply.github.com>
2020-07-01 17:29:46 -0700

    Create main.tf

---
+++ scaleperf-50resources/main.tf
@@ -0,0 +1,300 @@
+resource "null_resource" "example1" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example2" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example3" {
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example4" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example5" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example6" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example7" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example8" { 
+ provisioner "local-exec" { 
+ command = "ls -a" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example9" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example10" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example11" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example12" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example13" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example14" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example15" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example16" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example17" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example18" { 
+ provisioner "local-exec" { 
+ command = "ls -a" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example19" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example20" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example21" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example22" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example23" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example24" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example25" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example26" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example27" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example28" { 
+ provisioner "local-exec" { 
+ command = "ls -a" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example29" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example30" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example31" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example32" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example33" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example34" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example35" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example36" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example37" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example38" { 
+ provisioner "local-exec" { 
+ command = "ls -a" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example39" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example40" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example41" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example42" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example43" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example44" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example45" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example46" { 
+ provisioner "local-exec" { 
+ command = "ls" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example47" { 
+ provisioner "local-exec" { 
+ command = "find ." 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example48" { 
+ provisioner "local-exec" { 
+ command = "ls -a" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example49" { 
+ provisioner "local-exec" { 
+ command = "df" 
+ interpreter = ["sh", "-c"] 
+ } 
+}
+resource "null_resource" "example50" { 
+ provisioner "local-exec" { 
+ command = "pwd" 
+ interpreter = ["sh", "-c"] 
+ } 
+}

68c4c66722e86dffb5ed5e67db620a507980aa24
nisinghvmware <67397603+nisinghvmware@users.noreply.github.com>
2020-07-10 14:30:17 -0700

    Create main.tf

---
+++ terraform-250-resources/main.tf
@@ -0,0 +1,400 @@
+resource "aws_instance" "machine1" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine2" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine3" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine4" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine5" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine6" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine7" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine8" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine9" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine10" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine11" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine12" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine13" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine14" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine15" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine16" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine17" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine18" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine19" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine20" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine21" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine22" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine23" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine24" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine25" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine26" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine27" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine28" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine29" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine30" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine31" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine32" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine33" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine34" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine35" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine36" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine37" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine38" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine39" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine40" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine41" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine42" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine43" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine44" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine45" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine46" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine47" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine48" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine49" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine50" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine1" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine2" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine3" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine4" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine5" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine6" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine7" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine8" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine9" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine10" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine11" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine12" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine13" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine14" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine15" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine16" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine17" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine18" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine19" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine20" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine21" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine22" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine23" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine24" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine25" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine26" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine27" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine28" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine29" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine30" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine31" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine32" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine33" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine34" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine35" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine36" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine37" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine38" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine39" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine40" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine41" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine42" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine43" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine44" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine45" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine46" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine47" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine48" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine49" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}
+resource "aws_instance" "machine50" {
+ ami = "ami-04b9e92b5572fa0d1"
+ instance_type = "t2.micro"
+}

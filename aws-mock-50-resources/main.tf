69ffdf7acd6890b4ce924a63a7425414219b2f22
nisinghvmware <67397603+nisinghvmware@users.noreply.github.com>
2020-07-14 17:01:11 -0700

    Update main.tf

--- aws-mock-50-resources/main.tf
+++ aws-mock-50-resources/main.tf
@@ -205,7 +205,7 @@ resource "aws_instance" "machine48" {
  ami = "ami-04b9e92b5572fa0d1"
  instance_type = "t2.micro"
 }
-resource "aws_instance" "machine49" {
+resource "aws_instance" "machine499" {
  ami = "ami-04b9e92b5572fa0d1"
  instance_type = "t2.micro"
 }

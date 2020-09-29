8aed700f97add4f3dc1d7fa8fa0a19559bfe468a
nisinghvmware <67397603+nisinghvmware@users.noreply.github.com>
2020-09-28 19:06:55 -0700

    Update main.tf

--- vc02-ip/main.tf
+++ vc02-ip/main.tf
@@ -38,10 +38,7 @@ resource "vsphere_virtual_machine" "cloned_virtual_machine" {
     label = "disk0"
     size = 1
   }
-  timeouts {
-    create = "3h"
-    delete = "3h"
-  }
+
   guest_id = data.vsphere_virtual_machine.template.guest_id
   scsi_type = data.vsphere_virtual_machine.template.scsi_type
   network_interface {
@@ -51,6 +48,7 @@ resource "vsphere_virtual_machine" "cloned_virtual_machine" {
 	clone {
 	  template_uuid = data.vsphere_virtual_machine.template.id
 	  customize {
+		timeout = 120
 		linux_options {
 		  host_name = var.vm_name
 		  domain = "eng.vmware.com"

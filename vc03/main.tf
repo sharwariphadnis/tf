473aa795e1948269a301ebccfad26e4de5579611
nisinghvmware <67397603+nisinghvmware@users.noreply.github.com>
2020-12-15 11:34:48 -0800

    Update main.tf

--- vc03/main.tf
+++ vc03/main.tf
@@ -31,7 +31,7 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "tiny-photon-scale"
+  name          = "WebTinyCentOS65x86"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 

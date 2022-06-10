42bdcc597a0dc2c19a4d4dd1e247d803dbd8be76
Davinder2me <davinder2me@hotmail.com>
2022-06-09 19:20:16 -0700

    Update main.tf
    Update s3_use_path_style

--- aws-mock-instance/main.tf
+++ aws-mock-instance/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  s3_force_path_style         = true
+  s3_use_path_style           = true
   insecure = true
   skip_region_validation = true
   skip_get_ec2_platforms = true
